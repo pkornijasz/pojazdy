b93e3d19896b7463550e807dcb50b3bad975d168
pkornijasz <pk@nkk.pl>
2021-04-25 17:04:32 +0200

    Initial commit

---
+++ src/main/java/pl/kornijasz/pojazdy/PojazdyApplication.java
@@ -0,0 +1,13 @@
+package pl.kornijasz.pojazdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojazdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojazdyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kornijasz/pojazdy/PojazdyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kornijasz.pojazdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojazdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
